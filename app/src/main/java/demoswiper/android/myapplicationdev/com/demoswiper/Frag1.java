b772a0eb6d90421b1b1b693f376508e3597a522a
15004557 <H0peyur3gd7>
2017-07-27 11:58:29 +0800

    Initial commit

---
+++ app/src/main/java/demoswiper/android/myapplicationdev/com/demoswiper/Frag1.java
@@ -0,0 +1,35 @@
+package demoswiper.android.myapplicationdev.com.demoswiper;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag1 extends Fragment {
+
+    Button btnAddText;
+    TextView tvFrag1;
+
+
+
+    public Frag1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag1, container, false);
+    }
+
+}
